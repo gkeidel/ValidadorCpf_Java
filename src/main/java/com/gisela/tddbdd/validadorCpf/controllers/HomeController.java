98687dbabf191e5a623a8d995ebd18fa98b5428e
Gisela Keidel <giselakeidel@gmail.com>
2021-03-28 12:51:15 -0300

    Aplicacao funcionando com teste condicional

--- src/main/java/com/gisela/tddbdd/validadorCpf/controllers/HomeController.java
+++ src/main/java/com/gisela/tddbdd/validadorCpf/controllers/HomeController.java
@@ -1,7 +1,10 @@
 package com.gisela.tddbdd.validadorCpf.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import com.gisela.tddbdd.validadorCpf.models.Cliente;
+
 
 @Controller
 public class HomeController {
@@ -12,7 +15,9 @@ public class HomeController {
   }
 
   @GetMapping("/valida-cpf")
-  public String validaCpf(){
+  public String validaCpf(Cliente cliente, Model model){
+    boolean verdade = cliente.validarCPF();
+    model.addAttribute("verdade", verdade);
     return "home/cpfValidado";
   }
 }
